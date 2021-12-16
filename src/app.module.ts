bb936f5f3ed45400eddf6f0ed0632feb07899141
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2021-12-16 18:40:33 +0100

    Festivals

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FestivalsController } from './festivals.controller';
+import { FestivalsController } from './festivals/festivals.controller';
 
 @Module({
   controllers: [FestivalsController],
--- src/festivals/festivals.controller.ts
+++ src/festivals/festivals.controller.ts
@@ -3,54 +3,55 @@ import { Controller, Get } from '@nestjs/common';
 @Controller('festivals')
 export class FestivalsController {
   @Get()
-  findAll(): string {
-    return `
-    [
+  findAll(): any[] {
+    const cardCover =
+      'https://res.cloudinary.com/vkluchenkov/image/upload/v1639675384/sample.jpg';
+
+    return [
       {
         id: 1,
-        title: "Dance Weekend in Warsaw 2021",
-        type: "Virtual / Real",
-        startDate: "2021-08-18",
-        endDate: "2021-08-18",
-        location: "Warsaw, Poland",
+        title: 'Dance Weekend in Warsaw 2021',
+        type: 'Virtual / Real',
+        startDate: '2021-08-18',
+        endDate: '2021-08-18',
+        location: 'Warsaw, Poland',
         description:
           "Here's some description about the festival. And some more. Way more. Here's some description about the festival. And some more. Way more. Here's some description about the festival. And some more. Way more.",
         img: cardCover,
       },
       {
         id: 2,
-        title: "Some other festival 2021",
-        type: "Virtual",
-        startDate: "2020-08-18",
-        endDate: "2020-08-18",
-        location: "Hamburg, Germany",
+        title: 'Some other festival 2021',
+        type: 'Virtual',
+        startDate: '2020-08-18',
+        endDate: '2020-08-18',
+        location: 'Hamburg, Germany',
         description:
           "Here's some description about the festival. And some more. Way more. Here's some description about the festival. And some more. Way more. Here's some description about the festival. And some more. Way more.",
         img: cardCover,
       },
       {
         id: 3,
-        title: "Dance Weekend in Warsaw 2022",
-        type: "Virtual / Real",
-        startDate: "2022-08-18",
-        endDate: "2022-08-18",
-        location: "Warsaw, Poland",
+        title: 'Dance Weekend in Warsaw 2022',
+        type: 'Virtual / Real',
+        startDate: '2022-08-18',
+        endDate: '2022-08-18',
+        location: 'Warsaw, Poland',
         description:
           "Here's some description about the festival. And some more. Way more. Here's some description about the festival.",
         img: cardCover,
       },
       {
         id: 4,
-        title: "Some future festival 2022",
-        type: "Real",
-        startDate: "2022-09-21",
-        endDate: "2022-09-22",
-        location: "Tokyo, Japan",
+        title: 'Some future festival 2022',
+        type: 'Real',
+        startDate: '2022-09-21',
+        endDate: '2022-09-22',
+        location: 'Tokyo, Japan',
         description:
           "Here's some description about the festival. And some more. Way more. Here's some description about the festival.",
         img: cardCover,
       },
     ];
-    `;
   }
 }
