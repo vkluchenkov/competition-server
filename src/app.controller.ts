658af8d1f23f471f033a3a607d91aa512117a79d
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2022-03-10 10:05:37 +0100

    "/" rout

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   constructor() {}
+
+  @Get('/')
+  hello() {
+    return 'DWW Server v0.0.1';
+  }
 }
