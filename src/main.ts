aeec2b9c6787ea5acaa072fef5e2490517202fed
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2022-03-10 09:37:36 +0100

    cors3

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: 'http://localhost:3000' },
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: true,
+      optionsSuccessStatus: 204,
+    },
   });
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
